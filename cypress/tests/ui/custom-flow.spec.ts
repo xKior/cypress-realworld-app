fc3f3309cea2039c66141c86040d23d73bfe55c3
xKior <angelvegamartinez1@gmail.com>
2025-11-13 00:19:38 -0500

    se añadio mas tiempo

--- cypress/tests/ui/custom-flow.spec.ts
+++ cypress/tests/ui/custom-flow.spec.ts
@@ -23,13 +23,15 @@ describe('The Login Page', () => {
     cy.get('#password').type(`s3cret{enter}`);
 
     // Verificamos redirección con timeout mayor
-    cy.url({ timeout: 10000 }).should('not.include', '/signin');
+    cy.wait(5000)
+    cy.url({ timeout: 100000 }).should('not.include', '/signin');
 
     // Cookie de auth debe existir
     cy.getCookie('connect.sid').should('exist');
 
     // UI refleja usuario logueado - verificamos que el home esté visible
-    cy.get('[data-test="sidenav-home"]', { timeout: 10000 }).should('be.visible');
+    cy.wait(5000)
+    cy.get('[data-test="sidenav-home"]', { timeout: 100000 }).should('be.visible');
   });
 
   it('logs in using page object', function () {
